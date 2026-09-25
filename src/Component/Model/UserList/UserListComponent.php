e2330927198798c1d4c2dedd2405bda79dc37163
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2026-09-25 10:27:30 +0200

    Limpieza de datos de demo y meto plugin-token

--- src/Component/Model/UserList/UserListComponent.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Osumi\OsumiFramework\App\Component\Model\UserList;
-
-use Osumi\OsumiFramework\Core\OComponent;
-
-class UserListComponent extends OComponent {
-  public array $list = [];
-}
---
+++ src/Layout/DefaultLayoutTemplate.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<meta name="description" content="">
+		<title>{{title}}</title>
+		<link type="image/x-icon" href="/favicon.png" rel="icon">
+		<link type="image/x-icon" href="/favicon.png" rel="shortcut icon">
+	</head>
+	<body>
+		{{body}}
+	</body>
+</html>
